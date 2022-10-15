8fe4dc517c70ed5d7caa003d15632d41b1721bed
NomanAkhtarAnsari <68961174+NomanAkhtarAnsari@users.noreply.github.com>
2022-10-15 19:06:06 +0530

    migrations has been done (#4)
    Migrations

--- src/main/java/dashboard/SupplierDashboard.java
+++ src/main/java/dashboard/SupplierDashboard.java
@@ -2,8 +2,10 @@ package dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SupplierDashboard {
 
     public static void main(String[] args) {
